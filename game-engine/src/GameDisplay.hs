4139b72baef89d5c32214ec8e288ab25e950a588
Andrew Miller <andrew@amxl.com>
2017-12-29 12:28:58 +1100

    Change to one digit per cell

--- game-engine/src/GameDisplay.hs
+++ game-engine/src/GameDisplay.hs
@@ -24,27 +24,27 @@ showDigits i = [firstDigitOf i, lastDigitOf i]
 
 displayGame :: Game -> M.Map Coord Int -> String
 displayGame (Game { gameGridSize = gridSize, gameStatus = status }) cnt =
-    "  " ++ concatMap showDigits [0..(gridSize - 1)] ++ "\n" ++
-    concat [(firstDigitOf y) : " " ++ renderRow y ++ "\n" ++
-            (lastDigitOf y) : " " ++ renderRow y ++ "\n" |
+    "   " ++ map firstDigitOf [0..(gridSize - 1)] ++ "\n" ++
+    "   " ++ map lastDigitOf [0..(gridSize - 1)] ++ "\n" ++
+    concat [(showDigits y) ++ " " ++ renderRow y ++ "\n" |
             y <- [0..(gridSize - 1)]]
   where
     renderCoord :: Coord -> String
     renderCoord coord =
       case M.lookup coord status of
-        Just (CellStatus CellKnown CellMine) -> "**"
+        Just (CellStatus CellKnown CellMine) -> "*"
         Just (CellStatus CellKnown CellNotMine) ->
           case M.lookup coord cnt of
-            Just 0 -> "  "
-            Just 1 -> "11"
-            Just 2 -> "22"
-            Just 3 -> "33"
-            Just 4 -> "44"
-            Just 5 -> "55"
-            Just 6 -> "66"
-            Just 7 -> "77"
-            _ -> "88"
-        _ -> "??"
+            Just 0 -> " "
+            Just 1 -> "1"
+            Just 2 -> "2"
+            Just 3 -> "3"
+            Just 4 -> "4"
+            Just 5 -> "5"
+            Just 6 -> "6"
+            Just 7 -> "7"
+            _ -> "8"
+        _ -> "?"
     renderRow :: Int -> String
     renderRow y =
       concat [renderCoord (Coord x y) | x <- [0..(gridSize - 1)]]
